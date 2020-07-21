82619ecbbe4f9c1ba75bd0b5240a2143a8e56cd0
syckess <60026555+syckess@users.noreply.github.com>
2020-07-21 10:34:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,43 @@
 <?php
 
+    $method = $_SERVER['REQUEST_METHOD'];
+    // Process only when method is POST
 
-    echo "Hello world jajaja";
+    if($method == 'POST')
+    {
+        $requestBody = file_get_contents('php://input');
+        $json = json_decode($requestBody);
+        $text = $json->queryResult->parameters->text;
 
+        switch($text)
+        {
+            case 'hi':
+                $speech = "Hi, Nice to meet you";
+            break;
+            
+            case 'bye':
+                $speech = "Bye, good night";
+            break;
+            
+            case 'anything':
+                $speech = "Yes, you can type anything here.";
+            break;
+
+            default:
+                $speech = "Sorry, I dodnt get that, please ask something else";
+            break;
+        }
+        
+        $response = new \stdclass();
+        $response->fulfillmentText = $speech;
+        $response->displayText = $speech;
+        $response->source = "webhook";
+        echo json_encode($response);
+
+    }
+
+    else
+    {
+        echo "Method not allowed";
+    }
 ?>
