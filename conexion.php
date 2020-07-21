b40fc075d61aed087023fcc9af4287609f150909
syckess <60026555+syckess@users.noreply.github.com>
2020-07-21 18:52:07 -0400

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,20 @@
+  
+!--<?php
+
+$servername = "bfpefjoyqfutj3kawr98-mysql.services.clever-cloud.com:3306";
+$database = "bfpefjoyqfutj3kawr98";
+$username = "u9oqkqjvsiok9pxt";
+$password = "nJ5nsu5Xt5uhDqx21wHE";
+ Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+ Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+
+
+
+
+?>
