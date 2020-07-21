9389841cf1d221d754b4ed882805ca3a90fa6525
syckess <60026555+syckess@users.noreply.github.com>
2020-07-21 19:20:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -14,42 +14,5 @@
     else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
-    
-    $method = $_SERVER['REQUEST_METHOD'];
-
-    if($method == 'POST')
-    {
-        $requestBody = file_get_contents('php://input');
-        $json = json_decode($requestBody);
-        $text = $json->queryResult->parameters->text;
-
-        switch($text){
-            case 'hi':
-                $speech = "Hi, PORFIN SE CONECTO LA BD";
-            break;
-            
-            case 'bye':
-                $speech = "Bye, trataremos de tener listo lo de la BD la proxima vez que nos veamos";
-            break;
-            
-            case 'anything':
-                $speech = "Podras decir anything cuando la BD este lista";
-            break;
-
-            default:
-                $speech = "Sorry, no te escuche porque estoy buscando resolver lo de la BD";
-            break;
-        }
-       
-        $response = new \stdclass();
-        $response->fulfillmentText = $speech;
-        $response->displayText = $speech;
-        $response->source = "webhook";
-        echo json_encode($response);
-
-    }
     mysqli_close($conn);
-    else{
-        echo "Method not allowed";
-    }
 ?>
