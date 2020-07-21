f9c0fea7c90791279bfcd1525431f0da97dc5170
syckess <60026555+syckess@users.noreply.github.com>
2020-07-21 11:11:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 
-
-    $servername = "192.168.50.100";
-    $database = "BD_AGAPES_TDSI";
-    $username = "Mcastro";
-    $password = "XmK(qg0T";
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $database);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    echo "Connected successfully";
-
     $method = $_SERVER['REQUEST_METHOD'];
     // Process only when method is POST
 
