9ba941d7725b0cf2b18054a9824254aebd6350d9
syckess <60026555+syckess@users.noreply.github.com>
2020-07-21 19:33:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php
-include("conexion.php");
-$con=conexion();
-echo("Conexion Exitosa);
 
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    if($method == 'POST')
+    {
+        $requestBody = file_get_contents('php://input');
+        $json = json_decode($requestBody);
+        $text = $json->queryResult->parameters->text;
+
+        switch($text)
+        {
+            case 'hi':
+                $speech = "Hi, PORFIN SE CONECTO LA BD";
+            break;
+            
+            case 'bye':
+                $speech = "Bye, trataremos de tener listo lo de la BD la proxima vez que nos veamos";
+            break;
+            
+            case 'anything':
+                $speech = "Podras decir anything cuando la BD este lista";
+            break;
+
+            default:
+                $speech = "Sorry, no te escuche porque estoy buscando resolver lo de la BD";
+            break;
+        }
+        
+        $response = new \stdclass();
+        $response->fulfillmentText = $speech;
+        $response->displayText = $speech;
+        $response->source = "webhook";
+        echo json_encode($response);
+
+    }
+    else
+    {
+        echo "Method not allowed";
+    }
 ?>
