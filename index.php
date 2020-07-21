a15a2ec1eb9c17b9d0c7674fe9c7797168307e6f
syckess <60026555+syckess@users.noreply.github.com>
2020-07-21 19:23:00 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
 <?php
 
-    $servername = "bfpefjoyqfutj3kawr98-mysql.services.clever-cloud.com:3306";
-    $database = "bfpefjoyqfutj3kawr98";
-    $username = "u9oqkqjvsiok9pxt";
-    $password = "nJ5nsu5Xt5uhDqx21wHE";
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $database);
-    // Check connection
-    //$sql = "INSERT INTO acumulador (acum) VALUES ('1')";
-    if (mysqli_query($conn)) {
-      echo "conexion lograda";
-   } 
-    else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    if($method == 'POST')
+    {
+        $requestBody = file_get_contents('php://input');
+        $json = json_decode($requestBody);
+        $text = $json->queryResult->parameters->text;
+
+        switch($text)
+        {
+            case 'hi':
+                $speech = "Hi, PORFIN SE CONECTO LA BD";
+            break;
+            
+            case 'bye':
+                $speech = "Bye, trataremos de tener listo lo de la BD la proxima vez que nos veamos";
+            break;
+            
+            case 'anything':
+                $speech = "Podras decir anything cuando la BD este lista";
+            break;
+
+            default:
+                $speech = "Sorry, no te escuche porque estoy buscando resolver lo de la BD";
+            break;
+        }
+        
+        $response = new \stdclass();
+        $response->fulfillmentText = $speech;
+        $response->displayText = $speech;
+        $response->source = "webhook";
+        echo json_encode($response);
+
     }
-    mysqli_close($conn);
+    else
+    {
+        echo "Method not allowed";
+    }
 ?>
