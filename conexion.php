63004ee9bd7cb5f952c1896dbadac02a9b090e00
syckess <60026555+syckess@users.noreply.github.com>
2020-07-21 19:30:21 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,18 +1,24 @@
   
 <?php
-
-$servername = "bfpefjoyqfutj3kawr98-mysql.services.clever-cloud.com:3306";
-$database = "bfpefjoyqfutj3kawr98";
-$username = "u9oqkqjvsiok9pxt";
-$password = "nJ5nsu5Xt5uhDqx21wHE";
- Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
- Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+function conexion()
+{
+  
+  $servername = "bfpefjoyqfutj3kawr98-mysql.services.clever-cloud.com:3306";
+  $database = "bfpefjoyqfutj3kawr98";
+  $username = "u9oqkqjvsiok9pxt";
+  $password = "nJ5nsu5Xt5uhDqx21wHE";
+   Create connection
+  $conn = mysqli_connect($servername, $username, $password, $database);
+   Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
+  mysqli_close($conn);
+  
+  
 }
-echo "Connected successfully";
-mysqli_close($conn);
+
 
 
 
