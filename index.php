dc87787adda67fd5e235504151304b03d1691cbc
syckess <60026555+syckess@users.noreply.github.com>
2020-07-23 21:40:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,6 @@
             break;
             
             case 'si':
-                $speech = "holi";
                 $aux = 1;
             break;    
                 
@@ -52,6 +51,7 @@
          }
         else
         {
+        $sql = "INSERT INTO acumulador (acum) VALUES ('$punto')";
         $response = new \stdclass();
         $response->fulfillmentText = $speech;
         $response->displayText = $speech;
@@ -59,7 +59,7 @@
 
         echo json_encode($response);
         }
-        $sql = "INSERT INTO acumulador (acum) VALUES ('$punto')";
+        
         mysqli_query($conn, $sql, $total);
         mysqli_close($conn);
         
