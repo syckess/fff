cd9a90abc34077d595efaf0dcd5a61af329cc931
syckess <60026555+syckess@users.noreply.github.com>
2020-07-23 20:59:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,25 +7,38 @@
         $requestBody = file_get_contents('php://input');
         $json = json_decode($requestBody);
         $text = $json->queryResult->parameters->text;
+        
+        
+        $servername = "bfpefjoyqfutj3kawr98-mysql.services.clever-cloud.com:3306";
+        $database = "bfpefjoyqfutj3kawr98";
+        $username = "u9oqkqjvsiok9pxt";
+        $password = "nJ5nsu5Xt5uhDqx21wHE";
+        $conn = mysqli_connect($servername, $username, $password, $database);
 
         switch($text)
         {
             case 'hi':
                 $speech = "Hi, PORFIN SE CONECTO LA BD";
+                $punto = 5;
             break;
             
             case 'bye':
                 $speech = "Bye, trataremos de tener listo lo de la BD la proxima vez que nos veamos";
+                $punto = 10;
             break;
             
             case 'anything':
                 $speech = "Podras decir anything cuando la BD este lista";
+                $punto = 20;
             break;
 
             default:
                 $speech = "Sorry, no te escuche porque estoy buscando resolver lo de la BD";
             break;
         }
+        $sql = "INSERT INTO acumulador (acum) VALUES ('$punto')";
+        mysqli_query($conn, $sql);
+        mysqli_close($conn);
         
         $response = new \stdclass();
         $response->fulfillmentText = $speech;
